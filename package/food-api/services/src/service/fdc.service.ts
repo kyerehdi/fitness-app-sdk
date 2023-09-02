48eb6d67e773a87812c506654248b8e27193ae4b
Derrick Kyereh <69907881+kyerehdi@users.noreply.github.com>
2023-09-01 20:52:07 -0400

    Made fdc food endpoint

---
+++ package/food-api/services/src/service/fdc.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+// import { Deserialize, Serialize} from 'cerialize';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FdcService {
+  private apiKey = 'xfITlKQ9dE74iWGyGvlW0bAncnZnjjlKGLcQrsGR';
+  readonly resourceUrl = 'https://api.nal.usda.gov/fdc/v1/';
+
+  constructor(
+    private httpClient: HttpClient,
+    private domSanitizer: DomSanitizer
+  ) {}
+
+  searchFoodDetail(searchResult: string): Observable<any> {
+    return this.httpClient.get(
+      `${this.resourceUrl}foods/search?api_key=${this.apiKey}&query=${searchResult}`
+    );
+  }
+}
