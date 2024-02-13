c79b3fda2cc5219faa9f4b2eb3b34a9faf5bdd2f
Derrick Kyereh <69907881+kyerehdi@users.noreply.github.com>
2024-02-12 19:15:48 -0500

    Added new model

---
+++ package/models/user-workouts.ts
@@ -0,0 +1,28 @@
+import { autoserialize } from 'cerialize';
+
+
+export class UserWorkout {
+    @autoserialize
+    id: number;
+
+    @autoserialize
+    workout_name: string;
+
+    @autoserialize
+    date: Date;
+
+    @autoserialize
+    personid : string;
+
+    @autoserialize
+    workoutid : number;
+
+    @autoserialize
+    reps: number;
+
+    @autoserialize
+    intensity: string;
+
+    @autoserialize
+    weight: number;
+}
